0fe0efad72dbd86fb4b6b7289dc8e6c544fee0e7
BenSmile <benjkafirongo@gmail.com>
2025-07-18 14:30:19 +0100

    Rename system name

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -13,9 +13,9 @@ const Navbar = () => {
                         <div className="flex items-center gap-3">
                             <Image src="/logo.svg" alt='Rentiful Logo' width={24} height={24} className='w-6 h-6' />
                             <div className="text-xl font-bold">
-                                RENT
+                                LUKA
                                 <span className='text-secondary-500 font-light hover:!text-primary-300'>
-                                    IFUL
+                                    NDAKU
                                 </span>
                             </div>
                         </div>
