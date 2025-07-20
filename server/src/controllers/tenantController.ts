b6bb193cc3b6201154f22404a42557e5da1f2d3f
BenSmile <benjkafirongo@gmail.com>
2025-07-20 15:28:17 +0100

    Get tenant controller

---
+++ server/src/controllers/tenantController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express"
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
+
+export const getTenant = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { cognitoId } = req.params;
+
+        const tenant = await prisma.tenant.findUnique({
+            where: { cognitoId },
+            include: {
+                favorites: true
+            }
+        });
+
+        if (tenant) {
+            res.json(tenant)
+        } else {
+            res.status(404).json({ "message": "Tenant not found" });
+        }
+    } catch (error: any) {
+        console.log("Error when fetching tenant", error.message)
+        res.status(500).json({ "message": `Error when fetching tenant ${error.message}` })
+    }
+}
+
+export const createTenant = async (req: Request, res: Response): Promise<void> => {
+
+}
+
